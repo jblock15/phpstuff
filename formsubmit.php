0679dd92db47cc2d105390dfe19a7b51ff698a4a
esaygo <elena.saygo@gmail.com>
2016-04-05 17:17:26 -0700

    small changes

--- formsubmit.php
+++ formsubmit.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Survey Form</title>
-
+<title>Survey Form</title>
 <style type="text/css">
 
 fieldset{
-  height: 400px;
+  height: 300px;
   width: 300px
 }
 
+#return {
+  height: 25px;
+  width: 70px;
+  border-radius: 5px;
+  margin-left: 200px;
+  background-color: rgba(48, 79, 172, 0.6);
+}
+#return a {
+  text-decoration: none;
+  color: #fff;
+  padding: 5px;
+}
+
 </style>
+
 </head>
 <body>
   <fieldset>
@@ -19,6 +32,7 @@ fieldset{
     <p>Coding Dojo Location: <?php echo $_POST['location'];?></p>
     <p>Favorite Language: <?php echo $_POST['language'];?></p>
     <p>Comments: <?php echo $_POST['comment'];?></p>
+    <div id="return"><a href="surveyform.php">Go back!</a></div>
   </fieldset>
 
 </body>
