fbb19cf4e6bc72462704818b285ca856ea2623ed
Minh Nguyen <nguyenhminhp@gmail.com>
2016-04-06 17:27:26 -0700

    code clean up and styling

--- Day3/formsubmit1.php
+++ Day3/formsubmit1.php
@@ -2,31 +2,31 @@
 <html>
 <head>
   <title>Survey Form Submit</title>
-
-<style type="text/css">
-
-fieldset{
-  height: 400px;
-  width: 40%;
-}
-
-</style>
-<script>function MYALERT() {
-alert("Thank you for your information!!!!");} MYALERT()
-</script>
-
+  <style type="text/css">
+    fieldset{
+      height: 400px;
+      width: 40%;
+    }
+  </style>
+  <script>
+    function MYALERT() {
+      alert("Thank you for your information!!!!");
+    } 
+    MYALERT();
+  </script>
 </head>
 <body>
   <fieldset>
-    <legend></h2>Submitted Information:</h2></legend>
-    <p>Email: <?php echo $_POST['email'];?></p>
-    <p>First Name: <?php echo $_POST['first_name'];?></p>
-    <p>Last Name: <?php echo $_POST['last_name'];?></p>
-    <p>Password: <?php echo $_POST['password'];?></p>
-    <p>Confirm Password: <?php echo $_POST['confirm_password'];?></p>
-    <p>Birthdate: <?php echo $_POST['birthdate'];?></p>
-    <p>Profile Picture: <?php echo $_POST['picture'];?></p>
+    <legend></h2>
+      Submitted Information:
+    </h2></legend>
+    <p>Email: <?= $_POST['email'];?></p>
+    <p>First Name: <?= $_POST['first_name'];?></p>
+    <p>Last Name: <?= $_POST['last_name'];?></p>
+    <p>Password: <?= $_POST['password'];?></p>
+    <p>Confirm Password: <?= $_POST['confirm_password'];?></p>
+    <p>Birthdate: <?= $_POST['birthdate'];?></p>
+    <p>Profile Picture: <?= $_POST['picture'];?></p>
   </fieldset>
-
 </body>
 </html>
